6f91042e6acccebe64ed7f91f13fd4e1a8a89c5d
calvincetom <calvincetom@outlook.com>
2022-04-17 16:24:13 +0300

    Add feature for _printf in driver.c, add _putchar for checker error

--- _printf.c
+++ _printf.c
@@ -9,27 +9,45 @@
 
 int _printf(const char *format, ...)
 {
-	va_list original, copy;
-	int my_printed = 0, error = 0;
+	int my_print = 0, (*structype)(char *, va_list);
+	char q[3];
+	va_list pa;
 
-	if (format == NULL || *format == '\0')
-		return (0);
-
-	va_start(original, format);
-	va_copy(copy, original);
-
-	error = check_input(format, &original);
-
-	if (error != 0)
+	if (format == NULL)
+		return (-1);
+	q[2] = '\0';
+	va_start(pa, format);
+	_putchar(-1);
+	while (format[0])
 	{
-		return (error *-1);
+		if (format[0] == '%')
+		{
+			structype = driver(format);
+			if (structype)
+			{
+				q[0] = '%';
+				q[1] = format[1];
+				my_print += structype(q, pa);
+			}
+			else if (format[1] != '\0')
+			{
+				my_print += _putchar('%');
+				my_print += _putchar(format[1]);
+			}
+			else
+			{
+				my_print += _putchar('%');
+				break; 
+			}
+			format += 2;
+		}
+		else
+		{
+			my_print += _putchar(format[0]);
+			format++;
+		}
 	}
-
-	my_printed = print_output(format, &copy);
-
-	va_end(original);
-	va_end(copy);
-
-	return (my_printed);
+	_putchar(-2);
+	return (my_print);
 
 }
--- main.h
+++ main.h
@@ -2,59 +2,21 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stddef.h>
 #include <stdlib.h>
 
 /**
- *  * struct special_formats - Struct to select specifiers
- *   *
- *    * @c: data type
- *     * @f: The function associated
- *      */
-typedef struct special_formats
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
+ *
+ * Return: int 
+ */
+typedef struct structprint
 {
-	char *c;
-	void (*f)(va_list *, int, int *);
-} s_format;
-
-/**
- *  * union pointer - union variable to handle pointer byte to byte
- *   * @ptr: void pointer to store a pointer.
- *    * @c: char array to handle each byte in ptr.
- *     */
-typedef union pointer
-{
-	void *ptr;
-	char c[sizeof(void *)];
-} my_pointer;
-
-int _printf(const char *, ...);
-int check_input(const char *format, va_list *);
-int print_output(const char *format, va_list *);
-int check_for_format(char, va_list *, int, int *count);
-void print_char(va_list *, int, int *);
-int _putchar(char c);
-void print_string(va_list *, int, int *);
-void _puts(char *, int *);
-void print_int(va_list *, int, int *);
-void print_number(int, int *);
-
-/* Advanced */
-
-void print_string_reverse(va_list *, int, int *count);
-void print_rev(char *s, int *count);
-void print_string_rot13(va_list *, int, int *count);
-void rot13(char *s, int *count);
-void print_bin(va_list *, int, int *count);
-void p_bin(unsigned int, int *);
-void print_octal(va_list *, int, int *count);
-void p_octal(unsigned int, int *);
-void print_uint(va_list *, int, int *count);
-void p_uint(unsigned int, int *);
-void print_uhex(va_list *, int, int *count);
-void p_uhex(unsigned int, int *);
-void print_UHEX(va_list *, int, int *count);
-void p_UHEX(unsigned int, int *);
-void print_pointer(va_list *, int, int *count);
-void p_ptr(char *, int *);
-
-#endif /* MAIN.H */
+	char *q;
+	int (*u)(char *format, va_list);
+} structype;
+int _putchar(char ch);
+int _printf(char *format, ...);
+#endif
